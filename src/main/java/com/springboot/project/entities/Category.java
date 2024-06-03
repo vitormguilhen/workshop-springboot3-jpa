4ba80ddc12f26331efeb317604d0df24619c677a
Vitor Machado <vitormguilhen@gmail.com>
2024-06-03 16:21:24 -0300

    Many-tomany association with joinTable

--- src/main/java/com/springboot/project/entities/Category.java
+++ src/main/java/com/springboot/project/entities/Category.java
@@ -1,5 +1,6 @@
 package com.springboot.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,7 +18,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category() {
