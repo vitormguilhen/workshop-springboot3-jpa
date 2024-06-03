553fd61103e091b857218f1816b14394831b1e11
Vitor Machado <vitormguilhen@gmail.com>
2024-06-03 16:11:02 -0300

    Product

--- src/main/java/com/springboot/project/entities/Category.java
+++ src/main/java/com/springboot/project/entities/Category.java
@@ -3,7 +3,9 @@ package com.springboot.project.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -14,6 +16,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -39,6 +44,10 @@ public class Category implements Serializable {
         this.name = name;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -50,4 +59,6 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
