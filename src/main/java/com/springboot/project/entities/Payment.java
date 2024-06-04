198179ad5feec48631058c5506914c965803ae89
Vitor Machado <vitormguilhen@gmail.com>
2024-06-04 14:11:52 -0300

    Sub total & total

--- src/main/java/com/springboot/project/entities/Payment.java
+++ src/main/java/com/springboot/project/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.springboot.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
